44c998ec27c2d46e5cb0af042a9f2b2ddb888257
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-02 12:06:23 +0900

    fixed

--- SrcCode/js/dest_obj2.js
+++ SrcCode/js/dest_obj2.js
@@ -1,6 +1,6 @@
 let book = { title: 'JavaPoketReference', publish: '技術評論社', price: 2680,
-            ...other: { keywd: 'java SE 8', logo: 'logo.jpg'} };
-let { price, title, other:{ keywd }} = book;
+            other: { keywd: 'java SE 8', logo: 'logo.jpg'} };
+let { title, other, other: {keywd} }= book;
 
 console.log(title);
 console.log(other);
---
+++ SrcCode/js/dest_obj3.js
@@ -0,0 +1,5 @@
+let book = { title: 'JavaPoketReference', publish: '技術評論社', price: 2680 };
+let { title, publish: company} = book;
+
+console.log(title);
+console.log(company);
