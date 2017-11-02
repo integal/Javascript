e4d516521c328c26d271cf7526b15858ef697979
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-02 11:16:43 +0900

    更新

--- SrcCode/js/dest.js
+++ SrcCode/js/dest.js
@@ -1,7 +1,6 @@
 let data = [11,22,33,44,55,66,77];
-let [x0,x1,x2,x3,x4,x5,x6] = data;
+let [x0,x1,x2, ...other] = data;
 
 console.log(x0);
 console.log(x2);
-console.log(x4);
-console.log(x6);
+console.log(other);
