72b222f7533150ce0de6f5ea2eb019aaf6536db6
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-08 14:18:18 +0900

    updated

---
+++ SrcCode/js/switch-fallthrow.js
@@ -0,0 +1,12 @@
+var rank = 'B';
+switch(rank){
+  case 'A' :
+  case 'B' :
+  case 'C' :
+    console.log('Victory!');
+    break;
+  case 'D' :
+    default :
+    console.log('Defeat');
+    break;
+}
