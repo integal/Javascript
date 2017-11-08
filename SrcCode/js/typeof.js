0ccb2f0af500101f4070e09261181b45f846c752
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-08 13:22:27 +0900

    updated

---
+++ SrcCode/js/typeof.js
@@ -0,0 +1,14 @@
+var num = 1;
+console.log(typeof num);
+
+var str = 'hello';
+console.log(typeof str);
+
+var flag = true;
+console.log(typeof flag);
+
+var ary = ['java','script'];
+console.log(typeof ary);
+
+var obj = {x:1 ,y:2};
+console.log(typeof obj);
