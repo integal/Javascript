d5a00a52441b0fd8fed0035dcb7d15ccc89bae48
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-08 13:50:51 +0900

    updated

--- SrcCode/js/else-if.js
+++ SrcCode/js/else-if.js
@@ -1,8 +1,7 @@
 var x = 30;
-if (x > 20){
+if (x > 20)
   console.log('x is more 20');
-}else if(x > 10){
+else if(x > 10)
   console.log('x is more 10');
-}else{
+else
   console.log('x is no more than 10');
-}
