87dbe3c7dcf58cdbfe20524eb7e8a5430465b8b3
Saito Shuuhei <integal@users.noreply.github.com>
2017-11-08 14:14:26 +0900

    updated

---
+++ SrcCode/js/switch.js
@@ -0,0 +1,15 @@
+var rank = 'B';
+switch(rank){
+  case 'A' :
+    console.log('Your rank is A');
+    break;
+  case 'B' :
+    console.log('Your rank is B');
+    break;
+  case 'C' :
+    console.log('Your rank is C');
+    break;
+  default :
+    console.log('Your rank is nothing');
+    break;
+}
